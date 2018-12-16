f7c07621ead398f8be06294401c902086cef24d4
Jake Tripp <jake@jake.local>
2018-12-16 15:03:11 -0600

    Make Toast a functional component

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,24 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import Snackbar from "@material-ui/core/Snackbar";
 
-class Toast extends Component {
-
-  render(props) {
-    return (
-      <Snackbar
-        className="toast"
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        open={this.props.shouldShowToast}
-        onClose={this.props.onClose}
-        variant="success"
-        autoHideDuration={1500}
-        ContentProps={{
-          "aria-describedby": "message-id"
-        }}
-        message={<span id="message-id">Copied!</span>}
-      />
-    );
-  }
-}
+const Toast = props => (
+  <Snackbar
+    className="toast"
+    anchorOrigin={{ vertical: "top", horizontal: "right" }}
+    open={props.shouldShowToast}
+    onClose={props.onClose}
+    variant="success"
+    autoHideDuration={1500}
+    ContentProps={{
+      "aria-describedby": "message-id"
+    }}
+    message={<span id="message-id">Copied!</span>}
+  />
+);
 
 export default Toast;
