0ab8ba1913d56367ea345f10e76665ae9f699400
Jake Tripp <jake@jake.local>
2018-12-17 19:53:27 -0600

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 
 // styles
 import { MuiThemeProvider } from "@material-ui/core/styles";
@@ -15,8 +14,3 @@ ReactDOM.render(
   </MuiThemeProvider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
