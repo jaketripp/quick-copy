10551a5ad607da4fa957c7045bc11652f9dc9574
Jake Tripp <shmogens@gmail.com>
2019-06-29 14:35:17 -0500

    Try adding service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import * as serviceWorker from './serviceWorker';
 
 // styles
 import { MuiThemeProvider } from "@material-ui/core/styles";
@@ -14,3 +15,8 @@ ReactDOM.render(
   </MuiThemeProvider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
